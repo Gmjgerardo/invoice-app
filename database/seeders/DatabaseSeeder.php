7f5c5e7ba76db21bcadcdb02badbbe0f1174d1d7
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-01 13:57:52 -0600

    Creating Customer entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\Counter::factory(1)->create();
         \App\Models\Product::factory(5)->create();
+        \App\Models\Customer::factory(5)->create();
     }
 }
